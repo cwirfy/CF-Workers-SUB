2e1df837c6a9a8215d9e0f03574027846b3ee71d
cwirfy <cwirfy@proton.me>
2026-02-28 21:03:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,13 +12,13 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://798c9ead-f8af-4b04-96c1-e79cc2ce2cd5@43.203.93.133:56521?encryption=none&flow=xtls-rprx-vision&security=reality&sni=apple.com&fp=chrome&pbk=2lgxTCF6WAiQplFdbkw-LjQ_JbdBFF-B8tKX1dHUxiU&sid=490ca5c9&type=tcp&headerType=none#2512-KO_01-reality
-hysteria2://798c9ead-f8af-4b04-96c1-e79cc2ce2cd5@43.203.93.133:31186?security=tls&alpn=h3&insecure=1&sni=www.bing.com#2512-KO_01-hysteria2
-tuic://798c9ead-f8af-4b04-96c1-e79cc2ce2cd5:798c9ead-f8af-4b04-96c1-e79cc2ce2cd5@43.203.93.133:59569?congestion_control=bbr&udp_relay_mode=native&alpn=h3&sni=www.bing.com&allow_insecure=1&allowInsecure=1#2512-KO_01-tuic
-vless://9ed51d7e-68e5-46d2-b430-043dc684cc66@18.136.162.247:49405?encryption=none&flow=xtls-rprx-vision&security=reality&sni=apple.com&fp=chrome&pbk=CdeL7T_hCIzrx0L6gstcHemHc_HJ6DQf3EGxGOkNAjI&sid=12820a7a&type=tcp&headerType=none#2512-SG_02-reality
-hysteria2://9ed51d7e-68e5-46d2-b430-043dc684cc66@18.136.162.247:27846?security=tls&alpn=h3&insecure=1&sni=www.bing.com#2512-SG_02-hysteria2
-tuic://9ed51d7e-68e5-46d2-b430-043dc684cc66:9ed51d7e-68e5-46d2-b430-043dc684cc66@18.136.162.247:40791?congestion_control=bbr&udp_relay_mode=native&alpn=h3&sni=www.bing.com&allow_insecure=1&allowInsecure=1#2512-SG_02-tuic
-hysteria2://7f951532-dd6c-463f-99f0-d80bf762d37c@54.92.80.253:48754?&alpn=h3&insecure=1&mport=48754&sni=www.bing.com#2503-JP_03-hysteria2
+vless://9ed51d7e-68e5-46d2-b430-043dc684cc66@18.136.162.247:49405?encryption=none&flow=xtls-rprx-vision&security=reality&sni=apple.com&fp=chrome&pbk=CdeL7T_hCIzrx0L6gstcHemHc_HJ6DQf3EGxGOkNAjI&sid=12820a7a&type=tcp&headerType=none#SG02|移动-联通|reality
+hysteria2://9ed51d7e-68e5-46d2-b430-043dc684cc66@18.136.162.247:27846?security=tls&alpn=h3&insecure=1&sni=www.bing.com#SG02|移动-联通-电信|hy2
+tuic://9ed51d7e-68e5-46d2-b430-043dc684cc66:9ed51d7e-68e5-46d2-b430-043dc684cc66@18.136.162.247:40791?congestion_control=bbr&udp_relay_mode=native&alpn=h3&sni=www.bing.com&allow_insecure=1&allowInsecure=1#SG02|电信|tuic
+vless://798c9ead-f8af-4b04-96c1-e79cc2ce2cd5@43.203.93.133:56521?encryption=none&flow=xtls-rprx-vision&security=reality&sni=apple.com&fp=chrome&pbk=2lgxTCF6WAiQplFdbkw-LjQ_JbdBFF-B8tKX1dHUxiU&sid=490ca5c9&type=tcp&headerType=none#KO01|移动-联通|reality
+hysteria2://798c9ead-f8af-4b04-96c1-e79cc2ce2cd5@43.203.93.133:31186?security=tls&alpn=h3&insecure=1&sni=www.bing.com#KO01|移动-联通-电信|hy2
+tuic://798c9ead-f8af-4b04-96c1-e79cc2ce2cd5:798c9ead-f8af-4b04-96c1-e79cc2ce2cd5@43.203.93.133:59569?congestion_control=bbr&udp_relay_mode=native&alpn=h3&sni=www.bing.com&allow_insecure=1&allowInsecure=1#KO01|电信|tuic
+hysteria2://7f951532-dd6c-463f-99f0-d80bf762d37c@54.92.80.253:48754?&alpn=h3&insecure=1&mport=48754&sni=www.bing.com#JP03|移动-联通-电信|hy2
 `
 
 let urls = [];
